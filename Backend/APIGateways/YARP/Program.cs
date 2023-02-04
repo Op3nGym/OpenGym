b0226b547eddbdd781e79bcd74bb0ff7732c6fc7
Francesco Belacca <francesco.belacca@hotmail.it>
2023-02-04 18:12:41 +0000

    trying to correctly configure swa, yarp, dapr and graphql catalog to debug locally using all the pieces

--- Backend/APIGateways/YARP/Program.cs
+++ Backend/APIGateways/YARP/Program.cs
@@ -1,20 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddReverseProxy()
+builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
-// builder.Services.AddHealthChecks();
-// builder.Services.AddMvc();
-// builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseRouting();
-// app.MapHealthChecks("api/proxy/healthz");
 app.MapReverseProxy();
-
-// app.UseHttpsRedirection();
-// app.UseAuthorization();
 
 app.Run();
