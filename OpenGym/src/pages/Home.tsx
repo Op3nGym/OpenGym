fef8efbadc06a0b7fe17fb43e4dd70c5d07d5e78
Francesco Belacca <francesco.belacca@hotmail.it>
2023-02-26 11:22:54 +0000

    started integrating graphql in the FE

--- OpenGym/src/pages/Home.tsx
+++ OpenGym/src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import { useQuery, gql } from '@apollo/client';
+
+//TODO https://www.apollographql.com/docs/react/development-testing/static-typing/
+
+const GET_COURSES = gql`
+  query GetCourses {
+    courses {
+      courseId
+      title
+      credits
+    }
+  }
+`;
+
 
 const Home: React.FC = () => {
+  const { loading, error, data } = useQuery(GET_COURSES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  const results = data.courses.map(({ courseId, title, credits }) => (
+    <div key={courseId}>
+      <h3>{title}</h3>
+      <br />
+      <b>Credits:</b>
+      <p>{credits}</p>
+      <br />
+    </div>
+  ))
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +45,9 @@ const Home: React.FC = () => {
           </a>{' '}
           will be your guide.
         </p>
+        <br/>
+        <br/>
+        {results}
       </IonContent>
     </IonPage>
   );
